ae919f82ba42ffe7beb9cb53457ffba9721f9946
kangkongcheap <163854034+kangkongcheap@users.noreply.github.com>
2024-03-19 23:55:29 +0800

    3/19/2024 11:55pm

--- MIS/Admin/Admin.php
+++ MIS/Admin/Admin.php
@@ -3,85 +3,198 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Sidebar in Header</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap Icons CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <style>
-        /* Custom CSS for sidebar */
-        .sidebar {
-            background-color: #333;
-            color: white;
-            transition: 0.3s;
-        }
-
-        .sidebar a {
-            padding: 10px 15px;
-            text-decoration: none;
-            font-size: 20px;
-            color: white;
-            display: block;
-            transition: 0.3s;
-        }
-
-        .sidebar a:hover {
-            background-color: #555;
-        }
-
-        @media screen and (max-width: 768px) {
-            .sidebar {
-                position: fixed;
-                z-index: 1000;
-                width: 250px;
-                height: 100%;
-                top: 0;
-                left: -250px;
-                overflow-y: auto;
-            }
-
-            .sidebar.show {
-                left: 0;
-            }
-
-            .content {
-                margin-left: 250px;
-            }
-        }
-    </style>
+    <title>Sidebar With Bootstrap</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-YQp1wFdsy1Z3dCU5ym8nfcfJWIPSK1rYBprYO8r00ELIOknvRr4aRKeqWSS6I6Zh" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/admin.css">
 </head>
+<style>
+
+
+.main {
+        min-height: 100vh;
+        width: 100%;
+        overflow: hidden;
+        transition: all 0.35s ease-in-out;
+        background-color:  #e2e3dc;
+    }
+
+ /* dashboard*/
+    
+    .dashboard .box {
+        background-color:white;
+        border: 1px solid #dee2e6;
+        padding: 30px;
+        margin-bottom: 30px;
+        border-radius: 5px;
+        height: 200px;
+    }
+
+    .main-box {
+        background-color: #f8f9fa;
+        border: 1px solid #dee2e6;
+        padding: 30px;
+        margin-bottom: 40px;
+        border-radius: 5px;
+        height: auto;
+    }
+
+
+    .main-box h3 {
+        margin-bottom: 10px;
+        font-weight: 700;
+    }
+    h2{
+        font-size: 50px;
+        font-weight: 700;
+        text-align: center;
+        margin-bottom: 20px;
+        margin-top: 20px;
+    }
+
+    .small-box {
+        display: inline-block;
+        margin-inline: 10px;
+        margin-top: 5px;
+        background-color: #17a2b8;
+        color: white;
+        border-radius: 5px;
+        font-size: 15px;
+        width: 100px;
+        
+    }
+
+    .small-box span {
+        font-weight: bold;
+        font-size: 15px;
+    }
+    .main-box h3 i{
+        font-size: 35px;
+        margin-right: 10px;
+    }
+
+</style>
 
 <body>
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button class="toggle-btn" type="button">
+                <img src="../image/uccLogo.png" alt="Company Logo" class="logo-imig">
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">MIS</a>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="#"  id="profile" class="sidebar-link">
+                    <i class="fa-solid fa-user"></i></i>
+                        <span>Profile</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
+                    <i class="fa-solid fa-code-pull-request"></i>
+                        <span>Request</span>
+                    </a>
+                    <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                        <li class="sidebar-item">
+                            <a href="#" id="service" class="sidebar-link">Service</a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#"  id="equipment" class="sidebar-link">Equipment</a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#"  id="feedback"class="sidebar-link">Feedback</a>
+                        </li>
+                    </ul>
+                </li>
+           
+                <li class="sidebar-item">
+                    <a href="#" id="inquiry" class="sidebar-link">
+                    <i class="fa-solid fa-message"></i>
+                        <span>Inquiry</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" id="record" class="sidebar-link">
+                    <i class="fa-solid fa-folder"></i>
+                        <span>Record</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" id="account" class="sidebar-link">
+                        <i class="fa-solid fa-gear"></i>
+                        <span>User Account  </span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="#" id="logout" class="sidebar-link">
+                <i class="fa-solid fa-right-from-bracket"></i>
+                    <span>Logout</span>
+                </a>
+            </div>
+        </aside>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarNav" aria-controls="sidebarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="#">Your Logo</a>
-            <div class="collapse navbar-collapse" id="sidebarNav">
-                <div class="sidebar">
-                    <h2>My Sidebar</h2>
-                    <a href="#"><i class="bi bi-house-door"></i> Home</a>
-                    <a href="#"><i class="bi bi-person"></i> Profile</a>
-                    <a href="#"><i class="bi bi-gear"></i> Settings</a>
-                    <a href="#"><i class="bi bi-envelope"></i> Contact</a>
-                    <a href="#" class="btn btn-danger mt-3"><i class="bi bi-box-arrow-right"></i> Logout</a>
+
+
+
+
+        <div class="main">
+            <div class="dashboard">
+                <div class="container">
+                    <h2>Dashboard</h2>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="main-box text-center ">
+                                <h3><i class="fa-solid fa-user"></i></i>Service</h3>
+                                <div class="small-box">Pending<br><span id="pendingCount">0</span></div>
+                                <div class="small-box">On-going<br><span id="ongoingCount">0</span></div>
+                                <div class="small-box">Done<br><span id="doneCount">0</span></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="main-box text-center ">
+                                <h3><i class="fa-solid fa-wrench"></i>Equipment</h3>
+                                <div class="small-box">Pending<br><span id="pendingCount">0</span></div>
+                                <div class="small-box">On-going<br><span id="ongoingCount">0</span></div>
+                                <div class="small-box">Done<br><span id="doneCount">0</span></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="main-box text-center ">
+                                <h3><i class="lni lni-support"></i>Feedback</h3>
+                                <div class="small-box"></i>Pending<br><span id="pendingCount">0</span></div>
+                                <div class="small-box">On-going<br><span id="ongoingCount">0</span></div>
+                                <div class="small-box">Done<br><span id="doneCount">0</span></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="main-box text-center ">
+                                <h3><i class="fa-solid fa-user"></i></i>User</h3>
+                                <div class="small-box">Pending<br><span id="pendingCount">0</span></div>
+                                <div class="small-box">On-going<br><span id="ongoingCount">0</span></div>
+                                <div class="small-box">Done<br><span id="doneCount">0</span></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </nav>
-
-    <!-- Page content -->
-    <div class="content">
-        <h2>Page Content</h2>
-        <p>This is the main content of the page.</p>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script>
+        const hamBurger = document.querySelector(".toggle-btn");
 
-    <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        hamBurger.addEventListener("click", function() {
+            document.querySelector("#sidebar").classList.toggle("expand");
+        });
+    </script>
 </body>
 
 </html>
